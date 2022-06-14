1782d0be386a3308ada32008b72115c43e905ef9
Jeron <norejudeisa@gmail.com>
2022-06-14 12:07:18 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import Toastify from 'toastify-js'
+import 'toastify-js/src/toastify.css'
+import { registerSW } from 'virtual:pwa-register'
+import './main.scss'
+import './style.css'
+document.querySelector('#app').innerHTML = `
+<h1>Hello Vite!</h1>
+<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+<div class='container'>
+<h4>I am styled by scss </h4>
+</div>
+`
+const updateSW = registerSW({
+  onNeedRefresh() {
+    Toastify({
+      text: `<h4 style='display: inline'>An update is available!</h4>
+      <br><br>
+      <a class='do-sw-update'>Click to update and reload</a>  `,
+      escapeMarkup: false,
+      gravity: 'bottom',
+      onclick() {
+        updateSW(true)
+      },
+    }).showToast()
+  },
+  // onOfflineReady() {},
+})
+if ('serviceWorker' in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW()
+}
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'JTECH PWA',
+        short_name: 'JTECH',
+        description: 'Just testing vite pwa plugin',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: './public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+})
