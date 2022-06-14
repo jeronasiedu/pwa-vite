e9cb1efec6ed7e6f3dc18cebc692ac3ae0ccc654
Jeron <norejudeisa@gmail.com>
2022-06-14 13:02:15 +0000

    update

--- main.js
+++ main.js
@@ -2,28 +2,29 @@ import 'toastify-js/src/toastify.css'
 import { registerSW } from 'virtual:pwa-register'
 import './main.scss'
 import './style.css'
-document.querySelector('#app').innerHTML = `
-<h1>Hello Vite!</h1>
-<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-<div class='container'>
-<h4>I am doing the most </h4>
-</div>
-`
-// const updateSW = registerSW({
-//   onNeedRefresh() {
-//     Toastify({
-//       text: `<h4 style='display: inline'>An update is available!</h4>
-//       <br><br>
-//       <a class='do-sw-update'>Another update</a>  `,
-//       escapeMarkup: false,
-//       gravity: 'bottom',
-//       onclick() {
-//         updateSW()
-//       },
-//     }).showToast()
-//   },
-//   // onOfflineReady() {},
-// })
+// document.querySelector('#app').innerHTML = `
+// <h1>Hello Vite!</h1>
+// <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// <div class='container'>
+// <h4>I am doing the most </h4>
+// </div>
+// `
+const updateSW = registerSW({
+  onNeedRefresh() {
+    Toastify({
+      text: `<h4 style='display: inline'>An update is available!</h4>
+      <br><br>
+      <a class='do-sw-update'>Another update</a>  `,
+      escapeMarkup: false,
+      gravity: 'bottom',
+      onclick() {
+        updateSW()
+        alert('You clicked me')
+      },
+    }).showToast()
+  },
+  // onOfflineReady() {},
+})
 if ('serviceWorker' in navigator) {
   // && !/localhost/.test(window.location)) {
   registerSW()
